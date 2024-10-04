c09337a65f2c875413805e1339276d034175c322
Wardah-RL <wardah@radyalabs.id>
2024-10-04 10:54:42 +0700

    queue configuration

---
+++ src/Shared/DotnetApiTemplate.Shared.Abstractions/Models/QueueConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetApiTemplate.Shared.Abstractions.Models
+{
+  public class QueueConfiguration
+  {
+    public string Connection { get; set; } = string.Empty;
+  }
+}
