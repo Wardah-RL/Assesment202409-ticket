396ed3c55ec5e16be11e1b767fb7399c8c8c12d5
Wardah-RL <wardah@radyalabs.id>
2024-10-04 12:13:25 +0700

    migration

---
+++ src/DotnetApiTemplate.Persistence.Postgres/Migrations/20241004050050_addNoteTableTicketBroker.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetApiTemplate.Persistence.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNoteTableTicketBroker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "TrBookingTicketBroker",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "TrBookingTicketBroker");
+        }
+    }
+}
