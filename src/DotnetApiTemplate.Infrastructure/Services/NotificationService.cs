921b0b2df000049fdeb523c9066898fb9d692ce7
Wardah-RL <wardah@radyalabs.id>
2024-10-06 00:36:31 +0700

    create notification service

---
+++ src/DotnetApiTemplate.Infrastructure/Services/NotificationService.cs
@@ -0,0 +1,23 @@
+﻿using DotnetApiTemplate.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetApiTemplate.Infrastructure.Services
+{
+  public class NotificationService : INotification
+  {
+    public Task Execute(string queueName)
+    {
+
+
+
+
+
+
+      return Task.CompletedTask;
+    }
+  }
+}
