68b5a8c3fa24509233f7074b69d742c25834cd07
Wardah-RL <wardah@radyalabs.id>
2024-10-07 10:41:54 +0700

    add configuration MsEvent

---
+++ src/DotnetApiTemplate.Persistence.Postgres.Ticket/Configurations/TrPaymentConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using DotnetApiTemplate.Domain.Entities;
+using DotnetApiTemplate.Persistence.Postgres.Ticket.Entities;
+using DotnetApiTemplate.Shared.Abstractions.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetApiTemplate.Persistence.Postgres.Ticket.Configurations
+{
+  public class TrPaymentConfiguration : BaseEntityConfiguration<TrPayment>
+  {
+    protected override void EntityConfiguration(EntityTypeBuilder<TrPayment> builder)
+    {
+      builder.Property(e => e.Bank).HasMaxLength(100);
+      builder.Property(e => e.NamaPengirim).HasMaxLength(256);
+      builder.Property(e => e.NoRekening).HasMaxLength(50);
+    }
+  }
+}
